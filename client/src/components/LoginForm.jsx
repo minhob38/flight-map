8e518c44bc6de6e9f5a8c37420d8da0b889a8e37
kwon, minho <minhob38@gmail.com>
2021-10-04 21:42:37 +0900

    add password, email input

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import styled from "styled-components";;
+
+const LoginFormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  top: 0px;
+  left: 370px;
+  width: 350px;
+  height: 100%;
+  padding-top: 20px;
+  background-color: orange;
+`;
+
+const Form = styled.form`
+  display: flex;
+
+`
+
+const Input = styled.input`
+  all: unset;
+  display: block;
+  /* display: flex; */
+  /* justify-content: center; */
+  /* align-items: center; */
+  text-align: center;
+  width: 300px;
+  height: 3rem;
+  background-color: white;
+  font-size: 16px;
+  border-bottom: 1px solid black;
+`;
+
+export default function LoginForm() {
+  const handleChangeUserInput = (ev) => {
+    const { name, value } = ev.target;
+    setUserInput({...userInput, [name]: value})
+  };
+
+  const [userInput, setUserInput] = useState({
+    email: "",
+    password: "",
+  })
+
+  return (
+    <LoginFormContainer>
+      <form>
+        <Input
+          name="email"
+          type="text"
+          placeholder="email을 입력하세요 : )"
+          onChange={handleChangeUserInput}
+          value={userInput.email}
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="비밀번호를 입력하세요 : )"
+          onChange={handleChangeUserInput}
+          value={userInput.password}
+        />
+      </form>
+    </LoginFormContainer>
+  );
+}
--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,16 +1,24 @@
 import styled from "styled-components";
+import { useSelector } from "react-redux";
 import LoginButton from "./LoginButton";
+import LoginForm from "./LoginForm";
 
 const SideBarContainer = styled.div`
+  position: relative;
   width: 350px;
   height: 100%;
   background-color: blue;
 `;
 
 export default function SideBar() {
+  const isLoginClicked = useSelector(state => {
+    return state.auth.isLoginClicked
+  })
+
   return (
     <SideBarContainer>
       <LoginButton />
+      {isLoginClicked && <LoginForm />}
     </SideBarContainer>
   );
 }
