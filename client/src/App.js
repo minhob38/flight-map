927a30da7d4abdd9d91f890e5140f75e828cf261
kwon, minho <minhob38@gmail.com>
2021-10-04 16:33:47 +0900

    reset style

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,20 @@ import styled from "styled-components";
 const AppContainer = styled.div`
   display: flex;
   flex-direction: row;
-  height: 500px;
+  height: 100vh;
+  width: 100vw;
   background-color: red;
 `;
 
 const SideBar = styled.div`
   width: 350px;
-  height: 500px;
+  height: 100%;
   background-color: blue;
 `;
 
 const Map = styled.div`
   flex: 1;
-  height: 500px;
+  height: 100%;
   background-color: tomato;
 `;
 
