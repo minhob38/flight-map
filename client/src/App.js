e5331195ac58d74b16c26363b3c28b62de3d3231
kwon, minho <minhob38@gmail.com>
2021-10-04 16:20:02 +0900

    implement inintial main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 500px;
+  background-color: red;
+`;
+
+const SideBar = styled.div`
+  width: 350px;
+  height: 500px;
+  background-color: blue;
+`;
+
+const Map = styled.div`
+  flex: 1;
+  height: 500px;
+  background-color: tomato;
+`;
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <AppContainer>
+      <SideBar />
+      <Map />
+    </AppContainer>
+  );
 }
 
 export default App;
