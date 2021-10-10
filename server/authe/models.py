80fb44e0c72b027366c9e97906a5b032ace19f17
kwon, minho <minhob38@gmail.com>
2021-10-10 23:34:11 +0900

    implement sign up

--- server/authe/models.py
+++ server/authe/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class User(models.Model):
     id = models.AutoField(primary_key = True)
-    email = models.TextField()
+    email = models.TextField(unique = True)
     password = models.TextField()
-    create_data = models.DateTimeField()
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f'type:{type(self)}) id:{self.id} email:{self.email} password:{self.password} created_at:{self.created_at}'
--- server/authe/views.py
+++ server/authe/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
+from .models import User
 import json
-
+from django.utils import timezone
 
 @csrf_exempt
 def signup(request):
-    if request.method == "POST":
-        data = {"message": "success"}
-        return HttpResponse(json.dumps(data), content_type="application/json")
+    try:
+        if request.method == "POST":
+            body = json.loads(request.body)
+            email = body["email"]
+            password = body["password"]
+
+            is_user = User.objects.filter(email=email).exists()
+
+            if is_user:
+                data = {"status": "error", "message": "user already exists"}
+                return HttpResponseBadRequest(json.dumps(data), content_type="application/json")
+            else:
+                user = User(email=email, password=password, created_at=timezone.now())
+                user.save()
+                data = {"status": "success", "message": "user signed up"}
+                return HttpResponse(json.dumps(data), content_type="application/json")
+    except Exception:
+        data = {"status": "success", "message": "internal server error"}
+        return HttpResponseServerError(json.dumps(data), content_type="application/json")
