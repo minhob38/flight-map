f82bf8ac4b3e8aa4213341c85e95b756b89b417a
kwon, minho <minhob38@gmail.com>
2021-10-14 00:42:52 +0900

    implement bcrypt

--- server/authe/views.py
+++ server/authe/views.py
@@ -6,17 +6,16 @@ from .models import User
 import json
 from django.utils import timezone
 import os
+import bcrypt
 
 @csrf_exempt
 def signup(request):
     try:
-        JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 
         if request.method == "POST":
             body = json.loads(request.body)
             email = body["email"]
             password = body["password"]
-
 
             is_user = User.objects.filter(email=email).exists()
 
@@ -24,10 +23,13 @@ def signup(request):
                 data = {"status": "error", "message": "user already exists"}
                 return HttpResponseBadRequest(json.dumps(data), content_type="application/json")
             else:
-                user = User(email=email, password=password, created_at=timezone.now())
+                salt = bcrypt.gensalt()
+                hash = bcrypt.hashpw(password.encode('utf-8'), salt)
+                user = User(email=email, password=hash, created_at=timezone.now())
                 user.save()
                 data = {"status": "success", "message": "user signed up"}
+                JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
                 return HttpResponse(json.dumps(data), content_type="application/json")
-    except Exception:
-        data = {"status": "error", "message": "internal server error"}
+    except Exception as e:
+        data = {"status": "error", "message": str(e)}
         return HttpResponseServerError(json.dumps(data), content_type="application/json")
